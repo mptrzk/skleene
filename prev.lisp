8e6129b92e924486ea09bac8a27e88e90b84739c
Teletrup <saletrapotasowatosyf@gmail.com>
2023-02-19 09:08:32 +0100

    definitions

--- prev.lisp
+++ prev.lisp
@@ -36,7 +36,7 @@
 
 (defun re-alt (exprs str)
   (if exprs
-      (mlet (((val rst) (re-1 (car exprs) str)))
+      (mlet (((val rst) (re (car exprs) str)))
         (if val
             (values val rst)
             (re-alt (cdr exprs) str)))
@@ -45,27 +45,33 @@
 
 (defun re-seq (exprs str acc)
   (if exprs
-      (mlet (((val rst) (re-1 (car exprs) str)))
+      (mlet (((val rst) (re (car exprs) str)))
         (if val
             (re-seq (cdr exprs) rst (concatenate 'string acc val))
             (values nil rst)))
       (values acc str)))
 
 (defun re-rep (expr n str i acc)
-  (mlet (((val rst) (re-1 expr str)))
+  (mlet (((val rst) (re expr str)))
     (if val
         (re-rep expr n rst (+ i 1) (concatenate 'string acc val))
         (values (if (>= i n) acc) rst))))
 
 (defun re-not (expr str)
-  (if (re-1 expr str)
+  (if (re expr str)
       (values nil str)
       (if (zerop (length str))
           (values "" "")
           (values (subseq str 0 1)
                   (subseq str 1)))))
 
-(defun re-1 (expr str)
+(defparameter *re-defs*
+  '((alpha-c . (alt (ran #\a #\z)
+                  (ran #\A #\Z))) 
+    (num-c . (ran #\0 #\9))
+    (alphanum-c . (alt alpha-c num-c))))
+
+(defun re (expr str)
   (cond ((characterp expr) (re-ran expr expr str))
         ((listp expr)
          (case (car expr)
@@ -73,21 +79,34 @@
            (alt (re-alt (cdr expr) str))
            (seq (re-seq (cdr expr) str ""))  
            (rep (re-rep (cadr expr) (caddr expr) str 0 ""))
-           (not (re-not (cadr expr) str))))
+           (not (re-not (cadr expr) str)))) 
+        ((symbolp expr)
+         (let ((rec (assoc expr *re-defs*)))
+           (if rec
+               (re (cdr rec) str)
+               (error "invalid definition"))))
         (t (error "invalid expression"))))
 
-(assert (equal (re-1 #\a "") nil))
-(assert (equal (re-1 #\a "a") "a"))
-(assert (equal (re-1 #\a "ba") nil))
-(re-1 #\a "d")
-(re-1 #\a "ad")
+(assert (equal (re #\a "") nil))
+(assert (equal (re #\a "a") "a"))
+(assert (equal (re #\a "ba") nil))
+(re #\a "d")
+(re #\a "ad")
 
-(re-1 '(ran #\a #\c) "b")
-(re-1 '(alt #\a #\b) "e")
-(re-1 '(alt (ran #\a #\c) #\ó) "b")
-(re-1 '(seq #\a #\b) "abcd")
-(re-1 '(rep (seq #\a #\b) 0) "ababcd")
-(re-1 '(not (seq #\a #\b) 0) "ababcd")
-(re-1 '(not (seq #\a #\b) 0) "agabcd")
-(re-1 '(not (seq #\a #\b) 0) "fababcd")
+(re '(ran #\a #\c) "b")
+(re '(alt #\a #\b) "e")
+(re '(alt (ran #\a #\c) #\ó) "b")
+(re '(seq #\a #\b) "abcd")
+(re '(rep (seq #\a #\b) 0) "ababcd")
+(re '(not (seq #\a #\b)) "ababcd")
+(re '(not (seq #\a #\b)) "agabcd")
+(re '(not (seq #\a #\b)) "fababcd")
+
+(re '(seq (ran #\a #\z) 
+          (rep (alt (ran #\a #\z)
+                    (ran #\0 #\9))
+               0))
+    "1a")
+
+(re 'num-c "22")
 
